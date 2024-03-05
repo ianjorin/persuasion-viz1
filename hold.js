e14ec3c7b304d62b34e8f22d675a0e2d98df672d
ianjorin <israelwhiz@gmail.com>
2024-03-05 08:01:19 -0500

    first commit

---
+++ hold.js
@@ -0,0 +1,90 @@
+
+
+$(document).ready(async function () {
+    await loadFull(tsParticles);
+
+    $("#tsparticles")
+        .particles()
+        .init(
+            {
+                background: {
+                    color: {
+                        value: "#0d47a1",
+                    },
+                },
+                fpsLimit: 120,
+                interactivity: {
+                    events: {
+                        onClick: {
+                            enable: true,
+                            mode: "push",
+                        },
+                        onHover: {
+                            enable: true,
+                            mode: "repulse",
+                        },
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#ffffff",
+                    },
+                    links: {
+                        color: "#ffffff",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.5,
+                        width: 1,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 6,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                        },
+                        value: 80,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
+            },
+            function (container) {
+                // container is the particles container where you can play/pause or stop/start.
+                // the container is already started, you don't need to start it manually.
+            },
+        );
+
+    // or
+
+    $("#tsparticles")
+        .particles()
+        .ajax("particles.json", function (container) {
+            // container is the particles container where you can play/pause or stop/start.
+            // the container is already started, you don't need to start it manually.
+        });
+});
